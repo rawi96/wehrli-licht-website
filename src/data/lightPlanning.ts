a003a71b34ed22bf2e844ddb1154ab0fed922b47
Raphi <raphael@smartive.ch>
2023-05-05 13:40:51 +0200

    fix casing issue

---
+++ src/data/lightPlanning.ts
@@ -0,0 +1,11 @@
+export const lightPlanning = {
+  title: 'Lichtplanung',
+  primaryButton: {
+    text: 'Weitere Angebote anzeigen',
+    link: '/',
+  },
+  secondaryButton: {
+    text: 'Kontakt',
+    link: '/kontakt',
+  },
+}
