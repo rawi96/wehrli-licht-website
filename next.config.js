561fea2da474fa0317b5d623e4c12c00bf0fa790
Raphi <raphael@smartive.ch>
2023-05-09 13:12:58 +0200

    fix-pwa

--- next.config.js
+++ next.config.js
@@ -1,24 +1,16 @@
 const { withPlausibleProxy } = require('next-plausible')
 
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
 module.exports = withPlausibleProxy()(
   withPWA({
-    reactStrictMode: true,
-    pwa: {
-      dest: 'public',
-    },
-    eslint: {
-      dirs: ['src'],
-    },
-    async redirects() {
-      return [
-        {
-          source: '/shop',
-          destination: 'https://shop.wehrli.licht.ch',
-          permanent: true,
-        },
-      ]
-    },
+    // next.js config
   })
 )
