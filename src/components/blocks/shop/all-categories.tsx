ccec722ba5b24401fc805e3d4099d32721e8f4b4
Raphi <raphael@smartive.ch>
2024-11-21 14:04:00 +0100

    improve shop design

--- src/components/blocks/shop/all-categories.tsx
+++ src/components/blocks/shop/all-categories.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
 import { BlockWrapper } from '../../block-wrapper';
+import { Grid } from '@/components/grid';
 
 type Props = {
   block: AllCategoriesBlockFragment;
@@ -14,36 +15,32 @@ export const AllCategoriesBlock: FC<Props> = async ({ block: { disableMarginBott
 
   return (
     <BlockWrapper disableMarginBottom={disableMarginBottom} disableMarginTop={disableMarginTop}>
-      <div className="mx-auto max-w-2xl pb-20 lg:max-w-none">
-        <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-          {categories.map((category) => (
-            <Link key={category.slug} href={`/shop/kategorien/${category.slug}`} className="group block">
-              <div key={category.name} className="group relative cursor-pointer">
-                <div className="sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 relative mt-12 h-80 w-full overflow-hidden rounded bg-white sm:h-64">
-                  {category.images?.length && category.images[0].file?.url && (
-                    <Image
-                      src={category.images[0].file.url}
-                      alt={category.name}
-                      className="h-full w-full transform object-cover object-center transition-transform duration-300 group-hover:scale-105"
-                      height={100}
-                      width={1000}
-                    />
-                  )}
-                </div>
-                <h3 className="my-4 font-sans text-sm font-bold text-wehrli peer-[p&]:mt-6 md:text-base lg:text-lg lg:peer-[p&]:mt-8">
-                  {category.name}
-                </h3>
-                <p
-                  className="mt-2 text-xs"
-                  dangerouslySetInnerHTML={{
-                    __html: category.description,
-                  }}
-                ></p>
+      <Grid cols={categories.length > 3 ? 3 : categories.length}>
+        {categories.map((category) => (
+          <Link key={category.slug} href={`/shop/kategorien/${category.slug}`} className="group block">
+            <div className="group relative cursor-pointer">
+              <div className="sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 relative h-80 w-full overflow-hidden rounded bg-white sm:h-64">
+                {category.images?.length && category.images[0].file?.url && (
+                  <Image
+                    src={category.images[0].file.url}
+                    alt={category.name}
+                    className="h-full w-full transform object-cover object-center transition-transform duration-300 group-hover:scale-105"
+                    height={100}
+                    width={1000}
+                  />
+                )}
               </div>
-            </Link>
-          ))}
-        </div>
-      </div>
+              <h3 className="my-4 font-sans text-sm font-bold text-wehrli md:text-base lg:text-lg">{category.name}</h3>
+              <p
+                className="mt-2 text-xs"
+                dangerouslySetInnerHTML={{
+                  __html: category.description,
+                }}
+              ></p>
+            </div>
+          </Link>
+        ))}
+      </Grid>
     </BlockWrapper>
   );
 };
