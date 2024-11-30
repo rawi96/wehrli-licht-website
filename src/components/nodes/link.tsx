e07c877f1a128cf3c70e6f39ca4de5cd9ad82d5b
Raphi <raphael@smartive.ch>
2024-11-30 11:25:17 +0100

    increase size of links

--- src/components/nodes/link.tsx
+++ src/components/nodes/link.tsx
@@ -15,7 +15,7 @@ export const Link: FC<Props> = ({ children, href, target, download, rel, title }
     target={target ?? '_self'}
     download={download}
     rel={rel}
-    className="mr-8 mt-2 inline-block border-b-2 border-wehrli text-xxs font-bold text-wehrli no-underline transition-colors duration-150 hover:border-wehrli-700 hover:text-wehrli-700 md:text-sm lg:border-b-4 lg:text-base lg:peer-[a&]:ml-8"
+    className="text-s mr-8 mt-4 inline-block border-b-2 border-wehrli font-bold text-wehrli no-underline transition-colors duration-150 hover:border-wehrli-700 hover:text-wehrli-700 md:text-sm lg:border-b-4 lg:text-base lg:peer-[a&]:ml-8"
     title={title}
   >
     {children}
