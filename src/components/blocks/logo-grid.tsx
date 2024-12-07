2a790c488a4ea9c01343e83889fd238897b202a1
Raphi <raphael@smartive.ch>
2024-12-07 14:21:01 +0100

    logo grid links

--- src/components/blocks/logo-grid.tsx
+++ src/components/blocks/logo-grid.tsx
@@ -1,6 +1,7 @@
 import { LogoGridBlockFragment } from '@/graphql/generated';
 import { FC } from 'react';
 import { BlockWrapper } from '../block-wrapper';
+import Link from 'next/link';
 
 type Props = {
   block: LogoGridBlockFragment;
@@ -10,17 +11,20 @@ export const LogoGridBlock: FC<Props> = ({ block: { logos, disableMarginBottom, 
   <BlockWrapper disableMarginBottom={disableMarginBottom} disableMarginTop={disableMarginTop}>
     <div className="grid grid-cols-3 gap-8 rounded p-10 sm:gap-12 md:grid-cols-4 md:gap-16 xl:grid-cols-6">
       {logos?.map((logo) => (
-        // there is no advantage in using next/image here, because the logos are SVGs
-        // eslint-disable-next-line @next/next/no-img-element
-        <img
-          key={logo.id}
-          src={logo.url}
-          width={logo.width ?? undefined}
-          height={logo.height ?? undefined}
-          alt={logo.alt ?? 'Partner Logo'}
-          loading="lazy"
-          className="w-full"
-        />
+        <Link key={logo.id} href={logo.title ?? '#'} passHref legacyBehavior>
+          {/* there is no advantage in using next/image here, because the logos are SVGs */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <a target="_blank" rel="noopener noreferrer" className="block">
+            <img
+              src={logo.url}
+              width={logo.width ?? undefined}
+              height={logo.height ?? undefined}
+              alt={logo.alt ?? 'Partner Logo'}
+              loading="lazy"
+              className="w-full"
+            />
+          </a>
+        </Link>
       ))}
     </div>
   </BlockWrapper>
