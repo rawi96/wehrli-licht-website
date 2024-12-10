010bc6bdb8e46bfff348bdeca317c0349cfc35ca
Raphi <raphael@smartive.ch>
2024-12-10 13:45:35 +0100

    opening hours

--- src/components/formatted-text.tsx
+++ src/components/formatted-text.tsx
@@ -3,7 +3,9 @@ export type Props = {
 };
 
 export const FormattedText = ({ text }: Props) => {
-  if (!text) return null;
+  if (!text) {
+    return null;
+  }
 
   return (
     <div className="prose-invert mx-auto text-center text-sm leading-6">
@@ -15,7 +17,6 @@ export const FormattedText = ({ text }: Props) => {
           .custom-prose a {
             text-decoration: underline;
           }
-          
         `}
       </style>
       <div className="custom-prose" dangerouslySetInnerHTML={{ __html: text }} />
